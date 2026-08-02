c3e514f099f412113b74b11a80d53070e0473b4c
Jana-teach <jana.0523024@gmail.com>
2026-08-02 23:05:40 +0300

    Add unit test and complete missing features

--- Azka.NotificationPlatform.Application/Features/Dashboard/Queries/GetProviderSummaryQuery.cs
+++
@@ -1,9 +0,0 @@
-using Azka.NotificationPlatform.Application.DTOs;
-using MediatR;
-
-namespace Azka.NotificationPlatform.Application.Features.Dashboard.Queries;
-
-/// <summary>
-/// CQRS query: returns notification delivery statistics grouped by provider (FR-10).
-/// </summary>
-public sealed record GetProviderSummaryQuery : IRequest<ProviderSummaryDto>;
